c8343f719c69fb34b9b50ec73e72ad179649d0b5
yaroslavx <yakupov.yaroslav@icloud.com>
2022-07-12 23:46:10 +0300

    font fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import './fonts/SF-Pro.ttf'; 
+import './index.css'; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
--- src/pages/af.jsx
+++ src/pages/af.jsx
@@ -9,7 +9,7 @@ export default function AF() {
         <Fragment>
             <div className={classes.hud}><HudComponent/></div>
             <div className={classes.page}>
-                <div className={classes.container}>
+                <div className={classes.container} >
                     <div className={classes.vid} ><VideoComponentWithHud src={"https://player.vimeo.com/video/374286671?title=0&byline=0&portrait=0&transparent=0&autoplay=1"} width={960} height={591} title={1}/></div>
                     <Fragment><img className={classes.imgs} src={require('../imgs/af/af1.png')} alt='1'/></Fragment>
                     <Fragment><img className={classes.imgs} src={require('../imgs/af/af2.png')} alt='2'/></Fragment>

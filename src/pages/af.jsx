be25da39318fdd9c537542271905a69db4c2920c
yaroslavx <yakupov.yaroslav@icloud.com>
2022-07-14 21:10:20 +0300

    fix scroll

--- src/pages/af.jsx
+++ src/pages/af.jsx
@@ -2,17 +2,15 @@ import React, { Fragment } from 'react';
 import classes from '../styles/Pages.module.css'
 import HudComponent from '../components/hudComponent';
 import VideoComponentWithHud from '../components/videoComponentWithHud';
-import { useHorizontalScroll } from '../hooks/useHorizontalScroll';
 import SlideButton from '../components/UI/buttons/slideButton';
 
 
 export default function AF() {
-    const scrollRef = useHorizontalScroll()
     return (
         <Fragment>
             <div className={classes.hud}><HudComponent/></div>
             <div className={classes.page}>
-                <div className={classes.container} ref={scrollRef} style={{overflow: "auto" }} id="vidContainer">
+                <div className={classes.container} id="vidContainer">
                     <div className={classes.vid} ><VideoComponentWithHud src={"https://player.vimeo.com/video/374286671?title=0&byline=0&portrait=0&transparent=0&autoplay=1"} width={960} height={591} title={1}/></div>
                     <SlideButton/>
                     <Fragment><img className={classes.imgs} src={require('../imgs/af/af1.png')} alt='1'/></Fragment>
